647370c129b2fdf6d59a079aaa24a3d99df8731e
GYoung <ytok1108@aol.com>
2024-10-08 23:05:33 +0900

    Hello vulkano

---
+++ rust_lang/001_vulkano-rs/a02_base_code/src/main.rs
@@ -0,0 +1,14 @@
+struct HelloTriangleApplication {}
+
+impl HelloTriangleApplication {
+    pub fn initialize() -> Self {
+        Self {}
+    }
+
+    fn main_loop(&mut self) {}
+}
+
+fn main() {
+    let mut app = HelloTriangleApplication::initialize();
+    app.main_loop();
+}
